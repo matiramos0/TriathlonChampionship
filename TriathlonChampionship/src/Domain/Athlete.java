0f0f4913fdd8305b00b31c36ed5dcd8d9770600e
Mati <matias_ramos@gmail.com>
2024-06-14 16:00:03 -0300

    Implementation of Stats class and inherited Athlete classes

--- TriathlonChampionship/src/Domain/Athlete.java
+++ TriathlonChampionship/src/Domain/Athlete.java
@@ -1,5 +1,14 @@
 package Domain;
 
 public class Athlete {
-
+	
+	//Attributes
+	
+	private String name;
+	private String last;
+	private long dni;
+	private String nationality;
+	private char sex;
+	
+	
 }
---
+++ TriathlonChampionship/src/Domain/Competence.java
@@ -0,0 +1,5 @@
+package Domain;
+
+public class Competence extends Athlete{
+
+}
